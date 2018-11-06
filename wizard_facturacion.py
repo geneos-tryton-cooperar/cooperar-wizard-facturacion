567b748983351ad2837b8f25888facb06386a07c
Fernando J. Recci <reccifernando@gmail.com>
2018-11-06 11:15:40 -0300

    cambios en el wizard

--- wizard_facturacion.py
+++ wizard_facturacion.py
@@ -48,7 +48,7 @@ class CrearFacturacion(Wizard):
 
 
     def crear_facturas(self):
-        #import pudb;pu.db
+        import pudb;pu.db
         Asociadas = Pool().get('party.party')
         filtro_asociadas = [
                 ('asociada', '=', True), 
@@ -120,7 +120,7 @@ class CrearFacturacion(Wizard):
                 i.save()
             sale.save()
             '''
-            import pudb;pu.db
+
             #Avanzamos a presupuesto
             sale.invoice_address = sale.party.address_get(type='invoice')
             sale.shipment_address = sale.party.address_get(type='delivery')
